f2d8f4206803804d10e0a91ecadf6a24112aab8a
alvarosaavedrau <cloud@alvarosaavedra.es>
2024-12-08 11:33:35 +0100

    upgrade version and add tf min version

--- main.tf
+++ main.tf
@@ -2,9 +2,10 @@ terraform {
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = "2.43.0"
+      version = "2.45.0"
     }
   }
+  required_version = ">= 1.9.5"
 }
 
 provider "digitalocean" {
