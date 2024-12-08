42dec42f01583ea92b5207626d3888059556cfcb
alvarosaavedrau <cloud@alvarosaavedra.es>
2024-12-08 11:31:55 +0100

    removed unused files

--- project.tf
+++
@@ -1,6 +0,0 @@
-# resource "digitalocean_project" "playground" {
-#   name        = "CV"
-#   description = "Project to play with DigitalOcean infrastructure"
-#   purpose     = "Web Application"
-#   environment = "Development"
-# }
