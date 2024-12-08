65e68a65a42dbf98e1963b94c6e810983119be10
alvarosaavedrau <cloud@alvarosaavedra.es>
2024-12-08 11:28:48 +0100

    using debian in droplet + ssh new path

--- droplet.tf
+++ droplet.tf
@@ -1,14 +1,19 @@
 resource "digitalocean_droplet" "droplet" {
-  image      = "ubuntu-24-04-x64"
-  name       = "droplet-1"
-  region     = "fra1"
-  size       = "s-1vcpu-512mb-10gb"
-  user_data  = file("cloud-init-data.yaml")
+  image  = "debian-12-x64"
+  name   = "debian-12-x64"
+  region = "fra1"
+  size   = "s-1vcpu-512mb-10gb" // curl -X GET -H "Content-Type: application/json"-H "Authorization: Bearer $DIGITALOCEAN_TOKEN""https://api.digitalocean.com/v2/sizes"
+  # user_data  = file("cloud-init-data.yaml")
   ssh_keys   = [digitalocean_ssh_key.sshKey.fingerprint]
   monitoring = false
 }
 
 resource "digitalocean_ssh_key" "sshKey" {
   name       = "Thinkpad ssh key"
-  public_key = file("ssh/digitalocean.pub")
+  public_key = file("~/.ssh/digitalocean.pub")
+}
+
+resource "digitalocean_droplet_snapshot" "snapshot" {
+  droplet_id = digitalocean_droplet.droplet.id
+  name       = "debian-12"
 }
